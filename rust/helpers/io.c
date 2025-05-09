929c56df82e5a4aa337fcd4096f2181e165dc6c7
Daniel Almeida <daniel.almeida@collabora.com>
2025-05-09 17:29:47 -0300

    rust: io: mem: add a generic iomem abstraction
    Add a generic iomem abstraction to safely read and write ioremapped
    regions.
    
    The reads and writes are done through IoRaw, and are thus checked either
    at compile-time, if the size of the region is known at that point, or at
    runtime otherwise.
    
    Non-exclusive access to the underlying memory region is made possible to
    cater to cases where overlapped regions are unavoidable.
    
    Signed-off-by: Daniel Almeida <daniel.almeida@collabora.com>

--- rust/helpers/io.c
+++ rust/helpers/io.c
@@ -8,7 +8,12 @@ void __iomem *rust_helper_ioremap(phys_addr_t offset, size_t size)
 	return ioremap(offset, size);
 }
 
-void rust_helper_iounmap(void __iomem *addr)
+void __iomem *rust_helper_ioremap_np(phys_addr_t offset, size_t size)
+{
+	return ioremap_np(offset, size);
+}
+
+void rust_helper_iounmap(volatile void __iomem *addr)
 {
 	iounmap(addr);
 }
